01637e91e39492dc17ac27ea0dc8f50f14ea0e92
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2024-05-07 10:28:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1439,7 +1439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/creatorbeatz")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/Contact_infinity_bot")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
